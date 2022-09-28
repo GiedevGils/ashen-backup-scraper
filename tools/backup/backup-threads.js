61671c1c56137e95045cb2c1af3689c6ea0a3626
Giede van Gils <giedevangils@proton.me>
2022-09-28 23:38:08 +0200

    be able to backup all data from forums, only take relevant stuff

--- tools/backup/backup-threads.js
+++ tools/backup/backup-threads.js
@@ -3,53 +3,85 @@ const fs = require('fs')
 const { updateCurrentPage, startLog, endLog, addToDone } = require('../../util/logger.js')
 const { getPageContent } = require('../../util/request-processor.js')
 const path = require('path')
+const { addStyles } = require('../prettify-backup/prettify.js')
 
 main()
 
 async function main () {
-  const pages = JSON.parse(fs.readFileSync(path.join(__dirname, './forum.json')))
+  const forums = JSON.parse(fs.readFileSync(path.join(__dirname, './forum.json')))
 
-  startLog(-1)
+  startLog(Math.ceil(getCount(forums, 0) / 10))
 
   fs.cpSync(path.join(__dirname, '../tree-view/result.json'), path.join(__dirname, './forum.json'))
   const outputPath = path.join(__dirname, './output')
 
-  for (const page of pages) {
-    backup(outputPath, page)
+  for (const forum of forums) {
+    await backup(outputPath, forum)
   }
 
   endLog()
 }
 
 async function backup (parentPath, forum) {
+  const forumPath = `${parentPath}/${forum.name.toLowerCase().replaceAll(' ', '-')}`
+
+  fs.mkdirSync(forumPath, { recursive: true })
+
+  for (const subForum of forum.subForums) {
+    await backup(forumPath, subForum)
+  }
+
   for (const thread of forum.threads) {
-    console.log(thread.name)
-    const { name, url, nrOfPosts } = thread
+    const { name, url, nrOfPosts, by } = thread
 
     updateCurrentPage(thread)
 
-    console.log(`doing thread | ${name} - pgs: ${nrOfPosts}`)
+    console.log(`doing thread | ${name} - pgs: ${Math.ceil(nrOfPosts / 10)}`)
 
-    await fs.mkdirSync(parentPath, { recursive: true })
+    await fs.mkdirSync(forumPath, { recursive: true })
 
-    for (let idx = 1; idx <= nrOfPosts; idx++) {
-      const path = `${parentPath}/${name.toLowerCase().replace(' ', '_')}/page-${idx}.html`
+    for (let idx = 1; idx <= Math.ceil(nrOfPosts / 10); idx++) {
+      const path = `${forumPath}/${format(name).toLowerCase()}__${format(by)}`
+      const filePath = `${path}/page-${idx}.html`
 
-      if (fs.existsSync(path) && idx !== nrOfPosts) { // if it is the same page, re-get it for the fact that posts might have been updated
+      fs.mkdirSync(path, { recursive: true })
+
+      if (fs.existsSync(filePath) && idx !== nrOfPosts) { // if it is the same page, re-get it for the fact that posts might have been updated
         addToDone()
         continue
       }
 
       try {
-        const content = await getPageContent(`${url}/page/${idx}`, '.viewthread')
+        const content = await getPageContent(`${url}/page/${idx}`, '.row')
 
-        fs.writeFileSync(path, content[0].toString()) // should be only 1 element with class threadview
+        fs.writeFileSync(filePath, content.toString()) // todo fix add styles
       } catch (err) {
-        console.error(`error at ${name}, ${idx}`)
+        console.error(`error at ${name}, ${idx}: ${err}`)
         fs.appendFileSync('backup-fails.txt', `${new Date().toISOString()} | ${name}, ${idx} (${url}) | ${err.message}\n`)
       }
 
       addToDone()
     }
   }
+}
+
+function format (str) {
+  return (str + '').replace('/', '.').replaceAll(' ', '_').toLowerCase()
+}
+
+function getCount (forums) {
+  let count = 0
+
+  for (const forum of forums) {
+    for (const thread of forum.threads) {
+      if (thread.nrOfPosts) {
+        count += +thread.nrOfPosts
+      }
+    }
+
+    if (forum.subForums.length > 0) {
+      count += getCount(forum.subForums)
+    }
+  }
+  return count
 }
--- tools/prettify-backup/prettify.js
+++ tools/prettify-backup/prettify.js
@@ -1,35 +1,57 @@
 const fs = require('fs')
 const path = require('path')
 
-fs.cpSync(path.join(__dirname, '../header-duplication/stylesheets'), path.join(__dirname, 'stylesheets'), { recursive: true })
-fs.cpSync(path.join(__dirname, '../backup/output'), path.join(__dirname, 'threads'), { recursive: true })
+// fs.cpSync(path.join(__dirname, '../header-duplication/stylesheets'), path.join(__dirname, 'stylesheets'), { recursive: true })
+// fs.cpSync(path.join(__dirname, '../backup/output'), path.join(__dirname, 'threads'), { recursive: true })
 
 const styles = fs.readdirSync(path.join(__dirname, 'stylesheets'))
 
-let headers = '<head>'
+// let headers = '<head>'
 
-for (const style of styles) {
-  headers = headers + `<link href="../../stylesheets/${style}" type="text/css" rel="stylesheet">`
+// for (const style of styles) {
+//   headers = headers + `<link href="../../stylesheets/${style}" type="text/css" rel="stylesheet">`
+// }
+
+// headers = headers + '</head><body>'
+
+// const folders = fs.readdirSync(path.join(__dirname, 'threads'))
+
+// for (const folder of folders) {
+//   const files = fs.readdirSync(path.join(__dirname, `threads/${folder}`))
+
+//   for (const file of files) {
+//     const filepath = path.join(__dirname, `threads/${folder}/${file}`)
+
+//     const data = fs.readFileSync(filepath)
+//     const fd = fs.openSync(filepath, 'w+')
+//     const insert = Buffer.from(headers)
+
+//     fs.writeSync(fd, insert, 0, insert.length, 0)
+//     fs.writeSync(fd, data, 0, data.length, insert.length)
+//     fs.close(fd, err => { if (err) throw err })
+
+//     fs.appendFileSync(filepath, '</body>')
+//   }
+// }
+
+module.exports = {
+  addStyles,
 }
 
-headers = headers + '</head><body>'
+function addStyles (content) {
+  let page = ''
 
-const folders = fs.readdirSync(path.join(__dirname, 'threads'))
+  page += '<head>'
 
-for (const folder of folders) {
-  const files = fs.readdirSync(path.join(__dirname, `threads/${folder}`))
-
-  for (const file of files) {
-    const filepath = path.join(__dirname, `threads/${folder}/${file}`)
-
-    const data = fs.readFileSync(filepath)
-    const fd = fs.openSync(filepath, 'w+')
-    const insert = Buffer.from(headers)
-
-    fs.writeSync(fd, insert, 0, insert.length, 0)
-    fs.writeSync(fd, data, 0, data.length, insert.length)
-    fs.close(fd, err => { if (err) throw err })
-
-    fs.appendFileSync(filepath, '</body>')
+  for (const style of styles) {
+    page += `<link href="/home/ubuntu/dev/ashen-backup-tools/tools/prettify-backup/stylesheets/${style}" type="text/css" rel="stylesheet">`
   }
+
+  page += '</head><body>'
+
+  page += content
+
+  page += '</body>'
+
+  return page
 }
