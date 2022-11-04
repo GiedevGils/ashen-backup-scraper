e4046bc77edac50dfea350db51938febc4e57778
Giede van Gils <giedevangils@proton.me>
2022-11-04 17:37:29 +0100

    also add tags to each run

--- full-backup.sh
+++ full-backup.sh
@@ -3,17 +3,17 @@
 today=$(date +"%Y-%m-%d")
 startTime=$(date +"%Y-%m-%dT%T")
 
-# npm run tree-public
-# npm run backup
+npm run tree-public
+npm run backup
 
 endTime=$(date +"%Y-%m-%dT%T")
 
 git add .
 git commit -m "backup $today" -m "start: $startTime -- end: $endTime"
+git push
 
 git tag "v$startTime"
-
-git push
+git push --tags
 
 
 echo "Backup complete || start: $startTime -- end: $endTime"
