c252d63d5040ab015443eccc379771f408311d2e
Giede van Gils <giedevangils@proton.me>
2022-11-04 17:36:49 +0100

    backup 2022-11-04
    start: 2022-11-04T17:36:49 -- end: 2022-11-04T17:36:49

--- full-backup.sh
+++ full-backup.sh
@@ -1,12 +1,12 @@
 #! /bin/sh
 
 today=$(date +"%Y-%m-%d")
-startTime=$(date +"%Y-%m-%d %T")
+startTime=$(date +"%Y-%m-%dT%T")
 
 # npm run tree-public
 # npm run backup
 
-endTime=$(date +"%Y-%m-%d %T")
+endTime=$(date +"%Y-%m-%dT%T")
 
 git add .
 git commit -m "backup $today" -m "start: $startTime -- end: $endTime"
